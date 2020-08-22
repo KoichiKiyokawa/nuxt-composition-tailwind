c5695a307bf0e8d7e0deae19c40491c2b2afddc8
K.Kiyokawa <koichi20110068@gmail.com>
2020-08-22 04:49:48 +0000

    feat: add user show page

---
+++ src/pages/user/_id.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  p Your id is {{id}}.
+</template>
+
+<script lang="ts">
+import { defineComponent, useContext } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  setup() {
+    const { route } = useContext()
+    const { id } = route.value.params
+
+    return { id }
+  },
+})
+</script>
