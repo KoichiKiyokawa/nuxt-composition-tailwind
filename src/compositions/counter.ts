7192568abd278ca63c01c7024196df52d535f685
K.Kiyokawa <koichi20110068@gmail.com>
2020-08-22 01:56:33 +0000

    feat: add counter composition

---
+++ src/compositions/counter.ts
@@ -0,0 +1,13 @@
+import { ref } from '@nuxtjs/composition-api'
+
+export const useCounter = () => {
+  const count = ref(0)
+  const increment = () => {
+    count.value++
+  }
+  const decrement = () => {
+    if (count.value > 0) count.value--
+  }
+
+  return { count, increment, decrement }
+}
