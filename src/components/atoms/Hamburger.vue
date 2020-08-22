2fe757d94115eea8de29204e21bb8c3a72467cc2
K.Kiyokawa <koichi20110068@gmail.com>
2020-08-22 04:47:05 +0000

    feat: add Hamburger component

---
+++ src/components/atoms/Hamburger.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+.hamburger
+  .line(v-for="i in 3" :key="i")
+</template>
+
+<style lang="sass" scoped>
+.hamburger
+  @apply w-4 mr-4 cursor-pointer
+
+$lineThickness: 0.15rem
+.line
+  @apply w-full bg-white
+  height: $lineThickness
+  & + &
+    margin-top: $lineThickness
+</style>
