9c02655827a9783ea30dce51c7380338168feff9
K.Kiyokawa <koichi20110068@gmail.com>
2020-08-22 04:49:32 +0000

    feat: add Sidebar component

---
+++ src/components/organisms/Sidebar.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+.h-screen.shadow-2xl(:class="isOpen ? 'w-1/5' : 'w-0'")
+  p hoge
+</template>
+
+<script lang="ts">
+import { defineComponent, useContext } from '@nuxtjs/composition-api'
+// import { getModule } from 'vuex-module-decorators'
+
+// import SidebarStore from '@/store/sidebar'
+
+export default defineComponent({
+  setup() {
+    const { store } = useContext()
+    // const SidebarModule = getModule(SidebarStore)
+    return { isOpen: store.state.sidebar.isOpen }
+  },
+})
+</script>
